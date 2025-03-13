e684656e23c035aaa2dd051aa7ed7947574a9afd
Isaiah Guilfoile <Isaiah.Guilfoile@polaris.com>
2025-03-13 18:32:04 -0500

    Not working but runs

--- ImageStitch/vM2Stitch.py
+++ ImageStitch/vM2Stitch.py
@@ -68,11 +68,31 @@ def main():
     print(cols) # The column indices for each tile index
 
     # Perform image stitching
-    result_df,_ = m2stitch.stitch_images(images, rows, cols, pou = 50, row_col_transpose=False, ncc_threshold=75)
+    result_df,_ = m2stitch.stitch_images(images, rows, cols, pou = 50, row_col_transpose=False, ncc_threshold=1)
 
     print(result_df["y_pos"]) # The absolute y position of tiles
     print(result_df["x_pos"]) # The absolute x position of the tiles
 
+    # stitching example
+    result_df["y_pos2"] = result_df["y_pos"] - result_df["y_pos"].min()
+    result_df["x_pos2"] = result_df["x_pos"] - result_df["x_pos"].min()
+
+    size_y = images.shape[1]
+    size_x = images.shape[2]
+
+    stitched_image_size = (
+        result_df["y_pos2"].max() + size_y,
+        result_df["x_pos2"].max() + size_x,
+    )
+    stitched_image = np.zeros_like(images, shape=stitched_image_size)
+    for i, row in result_df.iterrows():
+        stitched_image[
+            row["y_pos2"] : row["y_pos2"] + size_y,
+            row["x_pos2"] : row["x_pos2"] + size_x,
+        ] = images[i]
+
+    result_image_file_path = path.join(script_path, "stitched_image.npy")
+    np.save(result_image_file_path, stitched_image)
 
 
 if __name__ == "__main__":
